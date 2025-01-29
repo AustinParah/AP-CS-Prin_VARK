b25f28bdaf8022d8cc666cdb4c74caea2b14d821
AustinParah <parahaustin@gmail.com>
2025-01-29 11:41:44 -0600

    update words for offline
    e

---
+++ wordle/words.py
@@ -0,0 +1,28 @@
+import requests
+import random
+import json
+
+url ="https://random-word-api.herokuapp.com/word"
+params = {'length': str(input('whats the length of the word boss? '))}
+
+def getWord():
+
+    response = requests.get(url, params)
+
+    if(response.status_code != 200):
+        print('unnable to pull from API -- using offline list')
+        
+
+    wordJSON = response.json()
+    return wordJSON[0]
+
+def RegenerateOfflineWordlist():
+    params = {'length': '5', 'number': '40'}
+
+    with open('offlineWords.json', 'w') as file:
+        json.dump(requests.get(url, params).json(), file)
+
+def getOfflineWord():
+    with open('offlineWords.json') as wordsJSON:
+        wordsList = json.load(wordsJSON)
+    return wordsList[random.randrange(0,39)]
